7e54dc2388b0782413bef444720a1a4db75d4a82
chisato920business <chisato920business@gmail.com>
2026-04-02 15:31:21 +0900

    Update store-session.js

--- api/store-session.js
+++ api/store-session.js
@@ -1,6 +1,14 @@
 const { createClient } = require("redis");
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://magic-barrel.com");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
   }
